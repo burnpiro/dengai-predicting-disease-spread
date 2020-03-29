1881db3c0b33d43b7b66ceca3cecb6c8f0f803d8
burnpiro <kemalpiro@gmail.com>
2020-03-29 13:15:15 +0200

    Fix data extraction (option not to categorise cols

--- preprocessing_helpers.py
+++ preprocessing_helpers.py
@@ -1,20 +1,27 @@
 import pandas as pd
+import tensorflow as tf
+import numpy as np
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from data_info import *
 from datetime import datetime
+
+LSTM_STEP = 6
+LSTM_FUTURE_TARGET = 6
+LSTM_HISTORY = 12
 
 
 def extract_data(train_file_path, columns, categorical_columns=CATEGORICAL_COLUMNS, categories_desc=CATEGORIES):
     # Read csv file and return
     all_data = pd.read_csv(train_file_path, usecols=columns)
-    # map categorical to columns
-    for feature_name in categorical_columns:
-        mapping_dict = {categories_desc[feature_name][i]: categories_desc[feature_name][i] for i in
-                        range(0, len(categories_desc[feature_name]))}
-        all_data[feature_name] = all_data[feature_name].map(mapping_dict)
+    if categorical_columns is not None:
+        # map categorical to columns
+        for feature_name in categorical_columns:
+            mapping_dict = {categories_desc[feature_name][i]: categories_desc[feature_name][i] for i in
+                            range(0, len(categories_desc[feature_name]))}
+            all_data[feature_name] = all_data[feature_name].map(mapping_dict)
 
-    # Change mapped categorical data to 0/1 columns
-    all_data = pd.get_dummies(all_data, prefix='', prefix_sep='')
+        # Change mapped categorical data to 0/1 columns
+        all_data = pd.get_dummies(all_data, prefix='', prefix_sep='')
 
     # fix missing data
     all_data = all_data.interpolate(method='linear', limit_direction='forward')
@@ -31,7 +38,35 @@ def preproc_data(data, norm_cols=cols_to_norm, scale_cols=cols_to_scale):
     # Scale year and week no but within (0,1)
     new_data[scale_cols] = MinMaxScaler(feature_range=(0, 1)).fit_transform(new_data[scale_cols])
 
-    return new_data
+def create_features_columns(data: pd.DataFrame, categorical_columns=CATEGORICAL_COLUMNS,
+                            numerical_columns=NUMERIC_COLUMNS):
+    feature_columns = []
+
+    for feature_name in categorical_columns:
+        vocabulary = data[feature_name].unique()
+        feature_columns.append(one_hot_cat_column(feature_name, vocabulary))
+
+    for feature_name in numerical_columns:
+        feature_columns.append(tf.feature_column.numeric_column(feature_name,
+                                                                dtype=tf.float32))
+
+    return feature_columns
+
+
+def make_input_fn(X, y, n_epochs=1, shuffle=True):
+    num_examples = len(y)
+
+    def input_fn():
+        dataset = tf.data.Dataset.from_tensor_slices((dict(X), y))
+        if shuffle:
+            dataset = dataset.shuffle(num_examples)
+        # For training, cycle thru dataset as many times as need (n_epochs=None).
+        dataset = dataset.repeat(n_epochs)
+        # In memory training doesn't use batching.
+        dataset = dataset.batch(num_examples)
+        return dataset
+
+    return input_fn
 
 
 def split_data(data, train_frac=TRAIN_DATASET_FRAC, label_column=LABEL_COLUMN, filter_cols=None):
