37393b1610fa11a0c02b61be6b02055991a1c52c
burnpiro <kemalpiro@gmail.com>
2020-06-08 18:01:08 +0200

    tpot test

--- tpot_test.py
+++ tpot_test.py
@@ -1,38 +1,50 @@
 from tpot import TPOTRegressor
 import pandas as pd
 from data_info import *
+from preprocessing_helpers import *
 from sklearn.model_selection import train_test_split
 
+new_sj_norm = [
+                'precipitation_amt_mm',
+                'reanalysis_air_temp_k',
+                'reanalysis_avg_temp_k',
+                'reanalysis_max_air_temp_k',
+                'reanalysis_min_air_temp_k',
+                'reanalysis_precip_amt_kg_per_m2',
+                'reanalysis_relative_humidity_percent',
+                'reanalysis_sat_precip_amt_mm',
+                'station_avg_temp_c',
+                'station_max_temp_c',
+                'station_min_temp_c',
+                'station_precip_mm'
+]
+new_sj_scale = [
+                   'weekofyear',
+]
 
-def extract_data(train_file_path, columns, categorical_columns=CATEGORICAL_COLUMNS, categories_desc=CATEGORIES,
-                 interpolate=True):
-    # Read csv file and return
-    all_data = pd.read_csv(train_file_path, usecols=columns)
-    if categorical_columns is not None:
-        # map categorical to columns
-        for feature_name in categorical_columns:
-            mapping_dict = {categories_desc[feature_name][i]: categories_desc[feature_name][i] for i in
-                            range(0, len(categories_desc[feature_name]))}
-            all_data[feature_name] = all_data[feature_name].map(mapping_dict)
+extra_sj_cols = [
+]
+new_sj_cols = [LABEL_COLUMN] + CATEGORICAL_COLUMNS + new_sj_norm + new_sj_scale + extra_sj_cols + [DATETIME_COLUMN]
+new_sj_cols_no_label = CATEGORICAL_COLUMNS + new_sj_norm + new_sj_scale + extra_sj_cols + [DATETIME_COLUMN]
 
-        # Change mapped categorical data to 0/1 columns
-        all_data = pd.get_dummies(all_data, prefix='', prefix_sep='')
+sj_datasets, sj_norm_scale = generate_lstm_data(
+    train_file,
+    single_step=True,
+    history_size=24,
+    cols=new_sj_cols,
+    norm_cols=new_sj_norm,
+    scale_cols=new_sj_scale,
+    extra_columns=extra_sj_cols,
+    prepend_with_file=train_file,
+    train_frac=1.0
+)
+sj_train_x, sj_train_y = sj_datasets[0][0]
+sj_train_x = sj_train_x.reshape(sj_train_x.shape[0], sj_train_x.shape[1] * sj_train_x.shape[2])
 
-    # fix missing data
-    if interpolate:
-        all_data = all_data.interpolate(method='linear', limit_direction='forward')
-
-    return all_data
-
-
-train_x = extract_data(train_file, columns=CSV_COLUMNS)
-train_y = train_x.pop(LABEL_COLUMN)
-print(len(train_x.to_numpy()))
-print(len(train_y.to_numpy()))
-X_train, X_test, y_train, y_test = train_test_split(train_x.to_numpy(), train_y.to_numpy(),
+X_train, X_test, y_train, y_test = train_test_split(sj_train_x, sj_train_y,
                                                     train_size=0.8, test_size=0.2, random_state=42)
 
-tpot = TPOTRegressor(scoring="neg_mean_absolute_error", generations=200, population_size=10, verbosity=2, random_state=42)
+tpot = TPOTRegressor(scoring="neg_mean_absolute_error", generations=10, population_size=80, verbosity=2, random_state=42)
 tpot.fit(X_train, y_train)
 print(tpot.score(X_test, y_test))
-tpot.export('tpot_denga_pipeline2.py')
+tpot.export('tpot_denga_pipeline3.py')
